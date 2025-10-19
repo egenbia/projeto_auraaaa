26507cbbfd39b1cd4bb8070f6787d475983417ad
Bia Egen <biahegen1@gmail.com>
2025-10-19 19:25:31 -0300

    bd

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,17 +1,3 @@
-CREATE DATABASE aura_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE aura_db;
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
-);
-
-usuariosINSERT INTO usuarios (nome, email, senha)
-VALUES ('bia', 'teste@aura.com', 
-        'abobora'); 
-
 ALTER TABLE usuarios ADD COLUMN bio TEXT;
 INSERT INTO usuarios (nome, email, senha, bio)
 VALUES ('Maria Silva', 'maria.silva@gmail.com', 'senha123', 'Pesquisadora e estudante de IA...');
@@ -19,3 +5,5 @@ VALUES ('Maria Silva', 'maria.silva@gmail.com', 'senha123', 'Pesquisadora e estu
 -- Adiciona coluna 'membro_desde' com data/hora de criação
 ALTER TABLE usuarios 
 ADD COLUMN membro_desde DATETIME DEFAULT CURRENT_TIMESTAMP;
+
+UPDATE usuarios SET senha = SHA2(senha, 256);
